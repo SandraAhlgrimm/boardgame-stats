a903d76fada439c6f48da54224de06de9e84c2b7
phazonoverload <phazonoverload@github.com>
2025-04-03 16:17:06 +0200

    feat(database): create tables and insert initial records for games, players, plays, and play_players

--- database/initialize.js
+++ database/initialize.js
@@ -7,21 +7,28 @@ async function initializeDb() {
 }
 
 async function createTables(db) {
+	// Create the 'games' table to store board game information
 	await db.exec(`CREATE TABLE IF NOT EXISTS games (
     boardgamegeek_id INTEGER PRIMARY KEY NOT NULL,
     title TEXT NOT NULL,
     notes TEXT
   )`)
+
+	// Create the 'players' table to store player information
 	await db.exec(`CREATE TABLE IF NOT EXISTS players (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL
   )`)
+
+	// Create the 'plays' table to store play session information
 	await db.exec(`CREATE TABLE IF NOT EXISTS plays (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     game_id INTEGER NOT NULL,
     notes TEXT,
     FOREIGN KEY (game_id) REFERENCES games (boardgamegeek_id)
   )`)
+
+	// Create the 'play_players' table to associate players with play sessions
 	await db.exec(`CREATE TABLE IF NOT EXISTS play_players (
     play_id INTEGER NOT NULL,
     player_id INTEGER NOT NULL,
@@ -32,6 +39,7 @@ async function createTables(db) {
 }
 
 async function createInitialRecords(db) {
+	// Insert initial records into the 'games' table
 	await db.run(`INSERT OR IGNORE INTO games (title, boardgamegeek_id, notes) VALUES 
     ('Android: Netrunner', 124742, 'Now out of print. 2 player asymmetrical card game. I have a lot of cards for this game and a sealed copy.'),
     ('CATAN', 13, NULL),
@@ -42,6 +50,7 @@ async function createInitialRecords(db) {
     ('Ticket to Ride: Berlin', 388036, '15 minute version of Ticket to Ride'),
     ('Tokaido', 123540, 'Weird as a 2-player.')`)
 
+	// Insert initial records into the 'players' table if it is empty
 	const playersCount = await db.get(`SELECT COUNT(*) as count FROM players`)
 	if (playersCount.count === 0) {
 		await db.run(`INSERT OR IGNORE INTO players (name) VALUES 
@@ -54,6 +63,7 @@ async function createInitialRecords(db) {
       ('Tobias Beckmann')`)
 	}
 
+	// Insert initial records into the 'plays' table if it is empty
 	const playsCount = await db.get(`SELECT COUNT(*) as count FROM plays`)
 	if (playsCount.count === 0) {
 		await db.run(`INSERT OR IGNORE INTO plays (game_id, notes) VALUES 
@@ -62,6 +72,7 @@ async function createInitialRecords(db) {
       (36218, 'Won by one point thanks to the witch!')`)
 	}
 
+	// Insert initial records into the 'play_players' table
 	await db.run(`INSERT OR IGNORE INTO play_players (play_id, player_id) VALUES 
     (1, 1), (1, 2),
     (2, 3), (2, 4), (2, 5),
