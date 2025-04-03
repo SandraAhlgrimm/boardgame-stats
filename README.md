998ee84b8286cf9cd60b621ab46856c9fec1bdb0
phazonoverload <phazonoverload@github.com>
2025-04-03 16:14:50 +0200

    feat(plays): implement CRUD operations for plays and associated players

--- README.md
+++ README.md
@@ -13,14 +13,12 @@ npm run app.js
 
 ## TODO
 
-- Add plays (vuln here)
-- Add plays seed data
 - Add BGG API data
 - Implement frontend
 - Implement security vulnerability such as db.all(`SELECT * FROM books WHERE author LIKE '%${author}'`)
 - Implement code error - SELECT * FROM non_existent_table
 - Implement code error - missing comma between endpoint params
-- Create issues for feature implementation (locations, grab more data [images] for frontend, add game comments),
+- Create issues for feature implementation (locations, grab more data [images] for frontend, winners/scores),
 - Create issues for language conversion
 - Script out add testing
 
--- database/initialize.js
+++ database/initialize.js
@@ -16,27 +16,56 @@ async function createTables(db) {
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL
   )`)
+	await db.exec(`CREATE TABLE IF NOT EXISTS plays (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    game_id INTEGER NOT NULL,
+    notes TEXT,
+    FOREIGN KEY (game_id) REFERENCES games (boardgamegeek_id)
+  )`)
+	await db.exec(`CREATE TABLE IF NOT EXISTS play_players (
+    play_id INTEGER NOT NULL,
+    player_id INTEGER NOT NULL,
+    PRIMARY KEY (play_id, player_id),
+    FOREIGN KEY (play_id) REFERENCES plays (id),
+    FOREIGN KEY (player_id) REFERENCES players (id)
+  )`)
 }
 
 async function createInitialRecords(db) {
 	await db.run(`INSERT OR IGNORE INTO games (title, boardgamegeek_id, notes) VALUES 
-  ('Android: Netrunner', 124742, 'Now out of print. 2 player asymmetrical card game. I have a lot of cards for this game and a sealed copy.'),
-  ('CATAN', 13, NULL),
-  ('Dominion', 36218, NULL),
-  ('Kodama: The Tree Spirits', 181810, NULL),
-  ('Pan Am', 303057, 'Got it for Nathaniel. Loved it so much I bought a copy for myself.'),
-  ('Railroad Ink Challenge', 367513, '1-n player roll and write game'),
-  ('Ticket to Ride: Berlin', 388036, '15 minute version of Ticket to Ride'),
-  ('Tokaido', 123540, 'Weird as a 2-player.')`)
+    ('Android: Netrunner', 124742, 'Now out of print. 2 player asymmetrical card game. I have a lot of cards for this game and a sealed copy.'),
+    ('CATAN', 13, NULL),
+    ('Dominion', 36218, NULL),
+    ('Kodama: The Tree Spirits', 181810, NULL),
+    ('Pan Am', 303057, 'Got it for Nathaniel. Loved it so much I bought a copy for myself.'),
+    ('Railroad Ink Challenge', 367513, '1-n player roll and write game'),
+    ('Ticket to Ride: Berlin', 388036, '15 minute version of Ticket to Ride'),
+    ('Tokaido', 123540, 'Weird as a 2-player.')`)
 
-	await db.run(`INSERT OR IGNORE INTO players (name) VALUES 
-  ('Kevin Lewis'),
-  ('Floor Drees'),
-  ('Nathaniel Okenwa'),
-  ('Jose Lewis'),
-  ('Myrsini Koukiasa'),
-  ('Tystan Williams'),
-  ('Tobias Beckmann')`)
+	const playersCount = await db.get(`SELECT COUNT(*) as count FROM players`)
+	if (playersCount.count === 0) {
+		await db.run(`INSERT OR IGNORE INTO players (name) VALUES 
+      ('Kevin Lewis'),
+      ('Floor Drees'),
+      ('Nathaniel Okenwa'),
+      ('Jose Lewis'),
+      ('Myrsini Koukiasa'),
+      ('Tystan Williams'),
+      ('Tobias Beckmann')`)
+	}
+
+	const playsCount = await db.get(`SELECT COUNT(*) as count FROM plays`)
+	if (playsCount.count === 0) {
+		await db.run(`INSERT OR IGNORE INTO plays (game_id, notes) VALUES 
+      (124742, 'Kevin played a custom deck as the corp. Game via Jinteki.net.'),
+      (13, NULL),
+      (36218, 'Won by one point thanks to the witch!')`)
+	}
+
+	await db.run(`INSERT OR IGNORE INTO play_players (play_id, player_id) VALUES 
+    (1, 1), (1, 2),
+    (2, 3), (2, 4), (2, 5),
+    (3, 1), (3, 6) `)
 }
 
 module.exports = { initializeDb }
